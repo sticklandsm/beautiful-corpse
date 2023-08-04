990444883d661d96808f26ece1dd62a1b02028c9
Sean Stickland <sticklandsm@gmail.com>
2023-08-04 16:23:14 +1200

    fixed some formatting on mobile

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -18,13 +18,10 @@ function App() {
 
   const howToPlayModalContent = (
     <div className="flex flex-col items-center justify-center dark:bg-slate-800 dark:text-gray-400 text-center max-w-3xl">
-      <div className="md:text-2xl text-lg dark ">
+      <div className="md:text-2xl text-s dark ">
         <p>
-          {`This is an online game based on the pen and paper game Telephone Pictionary. You'll be asked to either draw something specific, or describe a picture of something. Once submitted you'll be given a link to share your interpretation.
-         At the very start of the game someone will give a random prompt. It can be anything from a phrase or a movie title to an inside joke or friends name. Once submitted, you'll get a link to share with your friends. The first person will draw something based on that prompt then be given another link to share. The next person will have to guess what the prompt was based only on
-        that drawing. This goes on with more people drawing and guessing and
-        sharing, the drawings getting more and more purple monkey
-        dishwasher. When a few people have gone, you can go to the Check Game
+          {`This is game is based on the pen and paper game Telephone Pictionary. You'll be asked to either draw something specific, or describe a picture of something. Once submitted you'll be given a link to share your interpretation.
+         When a few people have gone, you can go to the Check Game
         section and see all the drawings and how they changed from the original
         prompt.`}
         </p>
@@ -50,8 +47,11 @@ function App() {
         <div className="app h-screen dark:bg-slate-800 dark:text-gray-400">
           <Header setDarkMode={setDarkMode} darkMode={darkMode} />
           <ClientRoutes />
+          <br />
+          <br />
+          <br />
+          <Footer showInstructionsAgain={sethowToPlayShowModal} />
         </div>
-        <Footer showInstructionsAgain={sethowToPlayShowModal} />
       </div>
       <Modal
         content={howToPlayModalContent}
